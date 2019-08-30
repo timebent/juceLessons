478be0bc7bfa93db09e0d8fe5e68f002f6e2d0f6
John Thompson <jthompson@georgiasouthern.edu>
2019-08-30 12:04:54 -0400

    Add files via upload

---
+++ SineWithClass/Source/PluginProcessor.cpp
@@ -0,0 +1,219 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SineWithClassAudioProcessor::SineWithClassAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+SineWithClassAudioProcessor::~SineWithClassAudioProcessor()
+{
+    
+    //test
+}
+
+//==============================================================================
+const String SineWithClassAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SineWithClassAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SineWithClassAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SineWithClassAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SineWithClassAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SineWithClassAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SineWithClassAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SineWithClassAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String SineWithClassAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SineWithClassAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void SineWithClassAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    for(int i = 0; i< 10; i++) {
+      sineVector.push_back(SinOsc(random.nextInt(1000) + 100, 0.01, sampleRate));
+    }
+}
+
+void SineWithClassAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SineWithClassAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SineWithClassAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+  
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    MidiBuffer processedMIDI;
+    int time;
+    MidiMessage m;
+    
+    for (MidiBuffer::Iterator i (midiMessages); i.getNextEvent (m, time);)
+    {
+        if (m.isNoteOn())
+        {
+            for(int j = 0; j < 10; j++) {
+                float freq = random.nextInt(1000) + 100;
+                sineVector[j].setFreq(freq);
+            }
+        }
+    }
+    
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    
+    for(int i = 0; i< buffer.getNumSamples(); i++) {
+        float currentSample = 0.0;
+        for(int sines = 0; sines < 10; sines++) {
+            currentSample += sineVector[sines].getSample();
+        }
+        
+    for (int channel = 0; channel < totalNumOutputChannels; ++channel) {
+        auto* channelData = buffer.getWritePointer (channel);
+        channelData[i] = currentSample;
+        }
+        
+    }
+}
+
+//==============================================================================
+bool SineWithClassAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* SineWithClassAudioProcessor::createEditor()
+{
+    return new SineWithClassAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SineWithClassAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SineWithClassAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SineWithClassAudioProcessor();
+}
---
+++ SineWithClass/Source/PluginProcessor.h
@@ -0,0 +1,114 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/**
+*/
+
+class SinOsc
+{
+public:
+    
+    SinOsc() {}
+    // ~SinOsc();
+    
+    SinOsc(float freq, float amp, double sampleRate) {
+        mSampleRate = sampleRate;
+        setFreq(freq);
+        mAmp = amp;
+        mPhase = 0;
+        calculatePhaseStep(freq);
+    }
+    
+    float getSample() {
+        float mSample = calculateAmp(mPhase);
+        incrementPhase(mPhase);
+        return mSample * mAmp;
+    }
+    void setFreq(float freq) {
+        mFreq = freq;
+        calculatePhaseStep(freq);
+    }
+    
+private:
+    void calculatePhaseStep(float freq) {
+        mPhaseStep = (twoPi * freq) / mSampleRate;
+    };
+    
+    float calculateAmp(float phase) {
+        return sin(phase);
+    }
+    
+    void incrementPhase(float phase) {
+        mPhase += mPhaseStep;
+        if(mPhase >= twoPi) {mPhase = mPhase - twoPi;}
+    }
+    
+    float mPhase;
+    float mPhaseStep;
+    float mFreq;
+    float mAmp;
+    double mSampleRate;
+    float mSample;
+    float twoPi = M_PI * 2;
+};
+
+class SineWithClassAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    SineWithClassAudioProcessor();
+    ~SineWithClassAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    std::vector<SinOsc> sineVector;
+
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SineWithClassAudioProcessor)
+    Random random = Random::getSystemRandom();
+    int timer = 0.0;
+};
